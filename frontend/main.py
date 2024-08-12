5d872520981fb06132f7629ac269bb63ecfe3a39
Lydia Lim <lydialimlihan@gmail.com>
2024-08-12 21:25:44 +0800

    add message flashing

--- frontend/main.py
+++ frontend/main.py
@@ -1,18 +1,71 @@
-from flask import Flask, request, jsonify, url_for, render_template
-from flask import redirect, url_for
+from flask import (
+    Flask,
+    flash,
+    request,
+    jsonify,
+    url_for,
+    render_template,
+    session,
+    redirect,
+    url_for,
+)
+from datetime import timedelta
 import time
 
+
 app = Flask(__name__)
+app.secret_key = "abc123"
+app.permanent_session_lifetime = timedelta(minutes=1)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    if "user" in session:
+        user = session["user"]
+        flash(f"You are logged in as {user}!", "info")
+    else:
+        user = "Guest"
+        flash(f"You are not logged in. Click on Login button above to login.", "info")
+    return render_template("index.html", user=user)
 
 
-@app.route("/<name>")
-def greet(name):
-    return render_template("index.html", name=name)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user  # super simple way to store user data as a dictionary
+        flash(f"Login successful, {user}!", "info")
+
+        return redirect(url_for("user", usr=user))
+
+    else:
+        if "user" in session:
+            flash(f"Already logged in!", "info")
+            return redirect(url_for("user", usr=session["user"]))
+
+        return render_template("login.html")
+
+
+@app.route("/user")
+def user():
+
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash(f"You are not logged in!", "info")
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+
+    flash(f"You have been logged out!", "info")
+
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 @app.route("/admin/")
